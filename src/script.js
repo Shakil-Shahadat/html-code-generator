8955b3d70a609b13b9fda6dec52743be3c8fed3e
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-14 15:31:29 +0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -3,3 +3,25 @@
 // Add target="_blank" to all anchor tags, v 2.1
 for ( let x of document.links ) x.setAttribute( 'target', '_blank' );
 
+function generateA()
+{
+	let part = '<a href="';
+
+	if ( document.querySelector( '.a_href' ).value === '' )
+	{
+		alert( 'href can\'t be empty!' );
+		document.querySelector( '.a_href' ).focus();
+		return;
+	}
+	else
+	{
+		part += document.querySelector( '.a_href' ).value + '"';
+	}
+
+	if ( document.querySelector( '.a_target' ).value !== 'None' )
+	{
+		part += ' target="' + document.querySelector( '.a_target' ).value + '"';
+	}
+
+	document.querySelector( '.generatedCode' ).innerHTML = part + '>';
+}
